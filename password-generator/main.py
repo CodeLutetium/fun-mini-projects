d7d75dbe18732447249ab1c370d7df3ff2beb3f6
CodeLutetium <61930995+CodeLutetium@users.noreply.github.com>
2024-05-03 10:12:23 +0800

    Initial commit for password-generator mini project

---
+++ password-generator/main.py
@@ -0,0 +1,11 @@
+def generate_passphrase(length: int, seed: str) -> str:
+    raise NotImplementedError
+
+def generate_password(length: int, seed: str) -> str:
+    raise NotImplementedError
+
+def main() -> None:
+    raise NotImplementedError
+
+if __name__ == "main":
+    main()
