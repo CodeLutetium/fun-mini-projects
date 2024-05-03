8171d5c0e516c807981d5d8b7da2a3c2e68c40c2
CodeLutetium <61930995+CodeLutetium@users.noreply.github.com>
2024-05-03 19:49:18 +0800

    Completed view passwords function

--- password-generator/main.py
+++ password-generator/main.py
@@ -1,152 +1,12 @@
-import numpy as np
 import random
 import pyperclip
-import getpass
 
-# For cryptography
-import base64
-import os
-from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-import scrypt
+# Import relevant modules
+from pwgen import generate_passphrase, generate_password
+from login_handler import LoginHandler
 
 
-def generate_passphrase(length: int, seed=None) -> str:
-    """Generates a passphrase of the given length using the given seed.
-
-    Args:
-        length (int): The number of words in the passphrase.
-        seed (int): The seed for the random number generator.
-
-    Returns:   
-        str: A passphrase of the given length.
-    """
-    # Check if length is valid
-    if length < 4:
-        print("Passphrase length should be at least 4. Default length of 4 selected.")
-        length = 4
-
-    if length > 20:
-        print("Passphrase is too long. Limit of 20 selected")
-        length = 20
-
-    rng_generator = np.random.default_rng(seed)
-    words = load_words()
-    MAX_INDEX = len(words) - 1
-    random_indexes = rng_generator.integers(0, MAX_INDEX, length)
-
-    passphrase = "-".join([words[i] for i in random_indexes])
-    return passphrase
-
-
-def generate_password(length: int, seed: str) -> str:
-    raise NotImplementedError
-
-
-def load_words() -> list[str]:
-    with open("wordlist.txt", "r") as f:
-        return f.read().splitlines()
-
-
-def check_pw_file() -> None:
-    """
-    Check if passwords.csv exists, if not create it."""
-    try:
-        with open("passwords.csv", "r") as f:
-            pass
-    except FileNotFoundError:
-        # Disclaimer
-        input("Disclaimer: This program was written for fun, please do not use it to generate/ store password for sensitive accounts. NEVER store passwords in plain text! Press enter to continue.")
-
-        create_master_pw()
-
-        # Create passwords.csv
-        with open("passwords.csv", "w") as f:
-            pass
-
-
-def store_password(passphrase: str) -> None:
-    # Check for existence of passwords.csv
-    check_pw_file()
-
-    # Retrieve salt
-    try:
-        with open("salt", "rb") as f:
-            salt = f.read()
-    except FileNotFoundError:
-        print("Salt not found.")
-        return
-    
-    # Create kdf object
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=salt,
-        iterations=480000
-    )
-
-    # Verify master password
-    master_password = getpass.getpass("Enter master password: ")
-    if not verify_master_pw(master_password, salt):
-        print("Master password is incorrect.")
-        return
-    
-    # Encrypt passphrase
-    key = base64.urlsafe_b64encode(kdf.derive(str.encode(master_password)))
-    f = Fernet(key)
-    passphrase = f.encrypt(str.encode(passphrase))
-
-    # Save encrypted passphrase to passwords.csv
-    website = input("Enter website: ")
-    with open("passwords.csv", "a") as f:
-        f.write(f"{website}, {passphrase}\n")
-
-    print("Passphrase saved to passwords.csv")
-
-
-def create_master_pw() -> None:
-    # Create salt and save it
-    salt = os.urandom(32)
-    with open("salt", "wb") as f:
-        f.write(salt)
-
-    # Get master password from user
-    isValidPassword = False
-    while isValidPassword != True:
-        master_password = getpass.getpass(
-            "Enter master password. The master password will be used to encrypt and decrypt your stored passwords. \nMaster password (at least 8 characters): ")
-
-        # @TODO Add function to check if master password is sufficiently strong
-        if len(master_password) < 8:
-            print("Master password should be at least 8 characters.")
-        else:
-            print("Master password successfully created")
-            isValidPassword = True
-            master_password = str.encode(master_password)
-
-    # Hash salted master password and store
-    hashed_pw = scrypt.hash(master_password, salt)
-    with open("master_pw", "wb") as f:
-        f.write(hashed_pw)
-        print("Master password successfully stored.")
-
-
-def verify_master_pw(master_password: str, salt: bytes) -> bool:
-    # Retrieve stored hashed master password
-    try:
-        with open("master_pw", "rb") as f:
-            stored_hashed_pw = f.read()
-            stored_hashed_pw = base64.urlsafe_b64encode(stored_hashed_pw)
-    except FileNotFoundError:
-        print("Master password not found.")
-        return False
-
-    # Hash user input and compare to stored hash
-    return stored_hashed_pw == scrypt.hash(str.encode(master_password), salt)
-
-
-def main() -> None:
+def generate_passphrase_main() -> None:
     length = input("Enter passphrase length (at least 4): ")
     try:
         passphrase_length = int(length)
@@ -165,11 +25,82 @@ def main() -> None:
     print("Passphrase automatically copied to clipboard.")
 
     response = input(
-        "Do you want to save your password to a text file? (y/n): ")
+        "Do you want to save your password to a text file? (y/[n]): ")
     if response.lower() == "y":
         store_password(passphrase)
     else:
         print("Password not saved")
+
+
+def generate_password_main() -> None:
+    raise NotImplementedError
+
+
+def view_passwords() -> None:
+    if login_handler.is_logged_in == False:
+        login_handler.login()
+
+    with open("passwords.csv", "r") as f:
+        passwords = f.readlines()
+        for password in passwords:
+            website, encrypted_passphrase = password.split(", ")
+            passphrase = login_handler.f.decrypt(encrypted_passphrase)
+            print(f"Website: {website}, Password: {passphrase.decode()}")
+
+
+def store_password(passphrase: str) -> None:
+    if login_handler.is_logged_in == False:
+        login_handler.login()
+
+    # Encrypt passphrase
+    passphrase = login_handler.f.encrypt(str.encode(passphrase))
+    passphrase = passphrase.decode()
+
+    # Save encrypted passphrase to passwords.csv
+    website = input("Enter website: ")
+    with open("passwords.csv", "a") as f:
+        f.write(f"{website}, {passphrase}\n")
+
+    print("Passphrase saved to passwords.csv")
+
+
+login_handler = LoginHandler()
+
+
+def main() -> None:
+    print((login_handler.master_password))
+    while True:
+        print("==============================")
+        print("{:-^30}".format("Password manager"))
+        print("==============================")
+        print(f"""
+Select an option:
+(1) Generate passphrase
+(2) Generate password
+(3) View stored passwords {"(login required)" if login_handler.is_logged_in is False else ""}
+Press any other key to exit
+              
+Login status: {"Logged in" if login_handler.is_logged_in else "Not logged in"}
+""")
+        if login_handler.has_master_password == False:
+            print("Press (4) or (L) to create a master password.")
+        elif login_handler.is_logged_in == False:
+            print("Press (4) or (L) to login.")
+
+        print("==============================")
+
+        response = input()
+        if response == "1":
+            generate_passphrase_main()
+        elif response == "2":
+            generate_password_main()
+        elif response == "3":
+            view_passwords()
+        elif response == "4" or response.lower() == "l" and login_handler.is_logged_in == False:
+            login_handler.login()
+        else:
+            print("Exiting program")
+            break
 
 
 if __name__ == "__main__":
