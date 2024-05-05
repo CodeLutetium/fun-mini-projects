52f7a7fe73f90654065e982694b9bc1a3b08b6ff
CodeLutetium <61930995+CodeLutetium@users.noreply.github.com>
2024-05-05 20:18:20 +0800

    Added simple symmetric key encryption using XOR cipher

--- simple-symmetric-key-encryption/main.py
+++ simple-symmetric-key-encryption/main.py
@@ -1,17 +1,35 @@
 def encrypt(key: str, msg: str) -> str:
-    # Simple symmetric key encryption using XOR of key and msg
-    raise NotImplementedError
+    """
+    Simple symmetric key encryption using XOR of key and msg
 
-def decrypt(key: str, token: str) -> str:
-    # Simple symmetric key decryption using XOR of key and token
-    raise NotImplementedError
+    Args:
+        key (str): key to be used for encryption
+        msg (str): message to be encrypted
+
+    Returns:
+        str: encrypted ciphertext
+    """
+    ciphertext = []
+    for i in range(len(msg)):
+        ciphertext.append(chr(ord(msg[i]) ^ ord(key[i % len(key)])))
+    return "".join(ciphertext)
+    
+
+def decrypt(key: str, ciphertext: str) -> str:
+    """
+    Simple symmetric key decryption using XOR of key and ciphertext
+    """
+    return encrypt(key, ciphertext)
+
 
 def main():
-    key = input("Enter key (longer is better): ")
+    key = input(
+        "Enter key. For best security, key should at least be as long as the message (see one time pad): ")
     msg = input("Enter message: ")
-    token = encrypt(key, msg)
-    print(f"Encrypted token: {token}")
-    print(f"Decrypted message: {decrypt(key, token)}")
+    ciphertext = encrypt(key, msg.strip())
+    print(f"Encrypted ciphertext: {ciphertext}")
+    print(f"Decrypted message: {decrypt(key, ciphertext)}")
+
 
 if __name__ == "__main__":
     main()
